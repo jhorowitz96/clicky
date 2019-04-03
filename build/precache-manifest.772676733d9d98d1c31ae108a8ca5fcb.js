5285d93a15d67ed1227906c339d284dfe4d6fe02
Greeley Wentz <jasonhorowitz@Jasons-MacBook-Pro.local>
2019-04-02 21:55:22 -0400

    github pages deploy

---
+++ build/precache-manifest.772676733d9d98d1c31ae108a8ca5fcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  },
+  {
+    "revision": "ba096481e2e033c2ad51",
+    "url": "/clicky/static/js/main.cdc9ee65.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "ba096481e2e033c2ad51",
+    "url": "/clicky/static/css/main.61327c79.chunk.css"
+  },
+  {
+    "revision": "5276a5cb258cf7c599669d9e23edbea6",
+    "url": "/clicky/index.html"
+  }
+];
